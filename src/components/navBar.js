5b3212891d5cbc1d7cf61c2b9463383ae39dcfaf
Akash <mishraakash273694@gmail.com>
2019-03-07 17:55:27 +0530

    minro changes

---
+++ src/components/navBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import InputBase from '@material-ui/core/InputBase';
+
+const navBar = (props)=>{
+        return(
+            <AppBar color="primary">
+                 <Toolbar>
+                    <IconButton  color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" >
+                        GitHub
+                    </Typography>
+                    {/* <Button color="inherit" className="float-right" >Login</Button> */}
+                    <InputBase
+                        placeholder="Search…"/>
+                </Toolbar>
+            </AppBar>
+        );
+}
+export default navBar;
