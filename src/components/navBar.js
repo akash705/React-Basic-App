024ef613e73afb59bd4eab7a9cb5797dae418a4d
Akash <mishraakash273694@gmail.com>
2019-03-07 18:32:37 +0530

    navbar done

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 import InputBase from '@material-ui/core/InputBase';
+import classes from './navbar.css';
 
-const navBar = (props)=>{
+class navBar extends Component{
+    render(){
         return(
             <AppBar color="primary">
                  <Toolbar>
-                    <IconButton  color="inherit" aria-label="Menu">
+                    {/* <IconButton  color="inherit" aria-label="Menu">
                         <MenuIcon />
-                    </IconButton>
+                    </IconButton> */}
                     <Typography variant="h6" color="inherit" >
                         GitHub
                     </Typography>
                     {/* <Button color="inherit" className="float-right" >Login</Button> */}
-                    <InputBase
+                    <InputBase className={classes.inputInput}
                         placeholder="Search…"/>
                 </Toolbar>
             </AppBar>
         );
+    }
 }
 export default navBar;
