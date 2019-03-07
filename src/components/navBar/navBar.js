308f0ebd3ade61a3ca51049540703b73e8e11831
Akash <mishraakash273694@gmail.com>
2019-03-07 20:44:04 +0530

    minor changes

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -2,6 +2,9 @@ import React,{Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import {fromEvent} from 'rxjs';
+import RootRef from '@material-ui/core/RootRef';
+
 // import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
@@ -9,7 +12,27 @@ import InputBase from '@material-ui/core/InputBase';
 import classes from './navbar.css';
 
 class navBar extends Component{
+    state={
+        value:''
+    }
+    subscription=undefined;
+    constructor(props){
+        super(props);
+        this.inputText = React.createRef();
+    }
+    componentDidMount(){
+        this.subscription = fromEvent(this.inputText.current,'input').subscribe(data=>{   
+            this.setState({value:data.target.value});
+        })
+    }   
+    componentWillUnmount(){
+        if(this.subscription){
+            this.subscription.unsubscribe();
+        }
+    }
+    
     render(){
+
         return(
             <AppBar color="primary">
                  <Toolbar>
@@ -20,8 +43,12 @@ class navBar extends Component{
                         GitHub
                     </Typography>
                     {/* <Button color="inherit" className="float-right" >Login</Button> */}
-                    <InputBase className={classes.inputInput}
-                        placeholder="Search…"/>
+                        <RootRef rootRef={this.inputText}>
+                                <InputBase className={classes.inputInput}
+                                placeholder="Search…"   value={this.state.value}
+                                ref={this.inputText}
+                                />
+                        </RootRef>
                 </Toolbar>
             </AppBar>
         );
