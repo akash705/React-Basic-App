93595cfafe200eed5fc0525378bc29b01d82612f
Akash <mishraakash273694@gmail.com>
2019-03-08 11:36:16 +0530

    actions

--- src/store/actionCreator/actions.js
+++ src/store/actionCreator/actions.js
@@ -3,13 +3,21 @@ import thunk from 'redux-thunk';
 const action=(data)=>{
     console.log('********************');
     // thunks code
-    return (dispatch)=>{
+    return (dispatch,getstate)=>{
     console.log('********************');
         setTimeout(() => {
+            console.log(getstate());
             dispatch( {
                 type:'requestComplete',
                 data:data
             });
+            setTimeout(data=>{
+                console.log('second dispatch');
+               dispatch({
+                   type:'requestComplete',
+                   data:[]
+               })
+            },1000)
         }, 2000);
     }
 
