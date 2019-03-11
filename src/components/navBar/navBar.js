dd4058e0149b52535bc2ab74e7cd5e8aa0fd6d7a
Akash mishra <mishraakash273694@gmail.com>
2019-03-11 20:08:52 +0530

    React version done

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -6,7 +6,7 @@ import {fromEvent, Observable } from 'rxjs';
 import { switchMap, throttleTime, debounceTime} from 'rxjs/operators';
 import RootRef from '@material-ui/core/RootRef';
 
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 import InputBase from '@material-ui/core/InputBase';
@@ -61,7 +61,13 @@ class navBar extends Component{
                     <Typography variant="h6" color="inherit" >
                         GitHub
                     </Typography>
-                    {/* <Button color="inherit" className="float-right" >Login</Button> */}
+                    
+                    <Button color="inherit" className="float-right" >
+                        <a href="https://fir-search-post.firebaseapp.com/" className={classes.link} target="_">
+                        Angular Version
+                        </a>
+                    </Button>
+                    {/* https://fir-search-post.firebaseapp.com/ */}
                         <RootRef rootRef={this.inputText}>
                                 <InputBase className={classes.inputInput}
                                 placeholder="Search…" onChange={this.change}   value={this.state.value}
